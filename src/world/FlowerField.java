168c57bf67c89192138460f14bc58d55fc834ae1
squirrel413 <nstibbels@gmail.com>
2025-04-13 16:59:09 -0400

    upload

--- src/world/FlowerField.java
+++ src/world/FlowerField.java
@@ -1,8 +1,18 @@
 package world;
 
+import bee.Worker;
+
 public class FlowerField {
 
     public final int MAX_WORKERS = 10;
 
     public FlowerField(){}
+
+    public void enterField(Worker worker) {
+
+    }
+
+    public void exitField(Worker worker) {
+
+    }
 }
